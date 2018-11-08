d5c3d829bdda224c7873fafee6d23c6c19acd2c0
Wei Hainan <weihainan@kuandeng.com>
2018-11-08 19:48:11 +0800

    增加测试脚本

---
+++ learn.py
@@ -0,0 +1,6 @@
+from keras.layers import Dense
+from keras.models import Sequential
+
+model = Sequential()
+model.add(Dense(units=64, activation='relu', input_dim=100))
+model.add(Dense(units=10, activation='softmax'))
